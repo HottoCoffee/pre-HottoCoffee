2b462bf7d90df0078d8272af4b84173b4df6c726
seito2 <seito1201@gmail.com>
2023-03-03 20:13:33 +0900

    [Front] Add argType for date

--- front/src/features/MonthlyCalendar/components/MonthlyCalendarFrame/MonthlyCalendarFrame.stories.tsx
+++ front/src/features/MonthlyCalendar/components/MonthlyCalendarFrame/MonthlyCalendarFrame.stories.tsx
@@ -5,12 +5,19 @@ import type { Meta, StoryObj } from "@storybook/react";
 const meta: Meta<typeof MonthlyCalendarFrame> = {
   title: "feature/MonthlyCalendar/MonthlyCalendarFrame",
   component: MonthlyCalendarFrame,
+  argTypes: {
+    date: {
+      control: {
+        type: "date",
+      },
+    },
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof MonthlyCalendarFrame>;
 
-export const HasNoChild: Story = {
+export const SimpleCalendar: Story = {
   args: {
     date: new Date("2023/3/1"),
     childBoxComponent: (date: Date) => {
