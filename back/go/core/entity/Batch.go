2120c2b22dcdb185e0c305a089e9c355beb1a187
T45K <tasktas9@gmail.com>
2023-03-14 23:07:19 +0900

    fmt

--- back/go/core/entity/Batch.go
+++ back/go/core/entity/Batch.go
@@ -3,12 +3,12 @@ package entity
 import "time"
 
 type Batch struct {
-  Id int
-  BatchName string
-  ServerName string
-  CronSetting string // TODO: use CronSetting-dedicated class
-  TimeLimit int
-  EsitimatedDuration int
-  StartDate time.Time
-  EndDate *time.Time
+	Id                 int
+	BatchName          string
+	ServerName         string
+	CronSetting        string // TODO: use CronSetting-dedicated class
+	TimeLimit          int
+	EsitimatedDuration int
+	StartDate          time.Time
+	EndDate            *time.Time
 }
