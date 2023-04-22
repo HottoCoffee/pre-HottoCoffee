4c87e1579158c8c6d08e7c7b2018804e1d6437d5
T45K <tasktas9@gmail.com>
2023-04-22 14:53:46 +0900

    deleted unused lines

--- frontend/src/features/BatchList/components/BatchCard/index.tsx
+++ frontend/src/features/BatchList/components/BatchCard/index.tsx
@@ -2,14 +2,6 @@ import { components } from "~/swagger/schema/schemas/batch";
 import styles from "./BatchCard.module.scss";
 
 type BatchType = components["schemas"]["Batch"];
-const batchMock: BatchType = {
-  id: 1,
-  batch_name: "エキスポート",
-  server_name: "初号機",
-  cron_setting: "0 0 * * *",
-  initial_date: "2023-01-01",
-  time_limit: 60,
-};
 
 interface Props {
   batch: BatchType;
