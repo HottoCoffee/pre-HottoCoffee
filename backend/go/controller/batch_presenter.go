3703a959f8fd03eefdfc237e6df08358832ed67a
T45K <tasktas9@gmail.com>
2023-04-29 15:01:56 +0900

    impl presenter

--- backend/go/controller/batch_presenter.go
+++ backend/go/controller/batch_presenter.go
@@ -24,6 +24,27 @@ func (p *BatchPresenter) SendBatchResponse(b entity.Batch) {
 	})
 }
 
+func (p *BatchPresenter) SendBatchesResponse(bs []entity.Batch) {
+	var response []map[string]interface{}
+	for _, b := range bs {
+		response = append(response, map[string]interface{}{
+			"id":                 b.Id,
+			"batch_name":         b.BatchName,
+			"server_name":        b.ServerName,
+			"cron_setting":       b.CronSetting.ToString(),
+			"initial_date":       b.StartDate,
+			"time_limit":         b.TimeLimit,
+			"estimated_duration": b.EstimatedDuration,
+		})
+	}
+
+	p.context.JSON(200, response)
+}
+
 func (p *BatchPresenter) SendNotFoundResponse() {
 	p.context.JSON(404, map[string]string{"status": "404", "message": "Not Found"})
+}
+
+func (p *BatchPresenter) SendInternalServerErrorResponse() {
+	p.context.JSON(500, map[string]string{"status": "500", "message": "Internal Server Error"})
 }
