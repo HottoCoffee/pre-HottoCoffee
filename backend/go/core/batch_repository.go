97957c93b1d3d817489e2ba7230eb7a041665710
T45K <tasktas@gmail.com>
2023-05-02 23:08:28 +0900

    update repository

--- backend/go/core/batch_repository.go
+++ backend/go/core/batch_repository.go
@@ -6,4 +6,5 @@ type BatchRepository interface {
 	FindById(id int) (*entity.Batch, error)
 	FindAll() ([]entity.Batch, error)
 	FindFilteredBy(query string) ([]entity.Batch, error)
+	Save(batch entity.Batch) (*entity.Batch, error)
 }
