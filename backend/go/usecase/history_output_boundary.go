c7aa5b17032db7d0f7a11620ba9b83ef3500f5fa
T45K <tasktas9@gmail.com>
2023-05-06 23:19:18 +0900

    add presenter

---
+++ backend/go/usecase/history_output_boundary.go
@@ -0,0 +1,8 @@
+package usecase
+
+import "github.com/HottoCoffee/HottoCoffee/core/entity"
+
+type HistoryOutputBoundary interface {
+	SendHistoryResponse(history entity.History)
+	SendNotFoundResponse()
+}
