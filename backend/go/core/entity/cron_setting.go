65bfb23d777220824e1af8cd7da13f06d3984978
T45K <tasktas9@gmail.com>
2023-06-08 18:54:38 +0900

    add Prev to CronSetting

--- backend/go/core/entity/cron_setting.go
+++ backend/go/core/entity/cron_setting.go
@@ -33,3 +33,111 @@ func (cs CronSetting) ListSchedules(begin time.Time, endInclusive time.Time) (sc
 	}
 	return schedules
 }
+
+// Prev copy from https://github.com/robfig/cron/pull/361
+func (cs CronSetting) Prev(t time.Time) time.Time {
+	s := cs.schedule.(*cron.SpecSchedule)
+
+	origLocation := t.Location()
+	loc := s.Location
+	if loc == time.Local {
+		loc = t.Location()
+	}
+	if s.Location != time.Local {
+		t = t.In(s.Location)
+	}
+
+	t = t.Add(-1*time.Second + time.Duration(t.Nanosecond())*time.Nanosecond)
+
+	added := false
+
+	yearLimit := t.Year() - 5
+
+WRAP:
+	if t.Year() < yearLimit {
+		return time.Time{}
+	}
+
+	for 1<<uint(t.Month())&s.Month == 0 {
+		cur := t.Month()
+		if !added {
+			added = true
+		}
+		t = time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, loc)
+		t = t.Add(-1 * time.Second)
+
+		if t.Month() > cur {
+			goto WRAP
+		}
+	}
+
+	for !dayMatches(s, t) {
+		cur := t.Day()
+		if !added {
+			added = true
+			t = time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, loc)
+			t = t.Add(-1 * time.Second)
+		} else {
+			t = t.AddDate(0, 0, -1)
+		}
+		if t.Day() > cur {
+			goto WRAP
+		}
+	}
+
+	for 1<<uint(t.Hour())&s.Hour == 0 {
+		cur := t.Hour()
+		if !added {
+			added = true
+			t = time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), 0, 0, 0, loc)
+			t = t.Add(-1 * time.Second)
+		} else {
+			t = t.Add(-1 * time.Hour)
+		}
+		if t.Hour() > cur {
+			goto WRAP
+		}
+	}
+
+	for 1<<uint(t.Minute())&s.Minute == 0 {
+		cur := t.Minute()
+		if !added {
+			added = true
+			t = t.Truncate(time.Minute)
+			t = t.Add(-1 * time.Second)
+		} else {
+			t = t.Add(-1 * time.Minute)
+		}
+
+		if t.Minute() > cur {
+			goto WRAP
+		}
+	}
+
+	for 1<<uint(t.Second())&s.Second == 0 {
+		cur := t.Second()
+		if !added {
+			added = true
+			t = t.Truncate(time.Second)
+		}
+		t = t.Add(-1 * time.Second)
+
+		if t.Second() > cur {
+			goto WRAP
+		}
+	}
+
+	return t.In(origLocation)
+}
+
+func dayMatches(s *cron.SpecSchedule, t time.Time) bool {
+	var (
+		domMatch = 1<<uint(t.Day())&s.Dom > 0
+		dowMatch = 1<<uint(t.Weekday())&s.Dow > 0
+	)
+	starBit := uint64(1 << 63)
+	if s.Dom&starBit > 0 || s.Dow&starBit > 0 {
+		return domMatch && dowMatch
+	}
+	return domMatch || dowMatch
+}
