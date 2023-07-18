e3a0930f7d5d9f77348f145bb45477fd89bda55f
T45K <tasktas9@gmail.com>
2023-07-18 23:41:47 +0900

    add sqlx deps

--- backend/rust/src/main.rs
+++ backend/rust/src/main.rs
@@ -1,9 +1,20 @@
 use axum::{Json, Router, routing::{get, post}};
 use axum::http::StatusCode;
+use chrono::{DateTime, Local, NaiveDateTime};
 use serde::Serialize;
+use sqlx::{MySqlPool, Row};
 
 #[tokio::main]
 async fn main() {
+    let pool = MySqlPool::connect(&"mysql://root:root@0.0.0.0:3306/hottocoffee")
+        .await
+        .unwrap();
+
+    let x = sqlx::query_as!(UserRecord, "select * from user")
+        .fetch_one(&pool)
+        .await.unwrap();
+    println!("{:?}", x);
+
     tracing_subscriber::fmt::init();
 
     let public_route = Router::new()
@@ -31,4 +42,15 @@ async fn sign_up() {
 struct User {
     id: u64,
     username: String,
+}
+
+#[derive(Debug)]
+struct UserRecord {
+    id: u32,
+    display_name: String,
+    email: Box<[u8]>,
+    password: Box<[u8]>,
+    created_at: NaiveDateTime,
+    updated_at: NaiveDateTime,
+    deleted_at: Option<NaiveDateTime>,
 }
