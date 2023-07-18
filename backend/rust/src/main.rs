10db845304857a0ab16b1e4dd58623601bfe4b18
T45K <tasktas9@gmail.com>
2023-07-18 22:15:40 +0900

    create skeleton

--- backend/rust/src/main.rs
+++ backend/rust/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("hello world")
+use axum::{Json, Router, routing::{get, post}};
+use axum::http::StatusCode;
+use serde::Serialize;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let public_route = Router::new()
+        .route("/sign-in", get(sign_in))
+        .route("/sign-up", post(sign_up));
+
+    let route = Router::new()
+        .nest("/public", public_route);
+
+    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
+        .serve(route.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn sign_in() -> (StatusCode, Json<User>) {
+    (StatusCode::OK, Json(User { id: 0, username: "".to_string() }))
+}
+
+async fn sign_up() {
+    todo!()
+}
+
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
 }
