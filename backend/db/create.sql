c2a86d2c53267619cdde3f95c0a7f3fa5d1c33c0
T45K <tasktas9@gmail.com>
2023-07-30 11:42:27 +0900

    move DB definition

--- backend/db/create.sql
+++ backend/db/create.sql
@@ -42,18 +42,6 @@ create table history (
     foreign key fk_workspace_id (batch_id) references workspace (id)
 ) engine = InnoDB default charset = utf8;
 
-create table user (
-    id int unsigned not null auto_increment,
-    display_name varchar(255) not null,
-    email blob not null,
-    password blob not null,
-    created_at datetime not null default current_timestamp,
-    updated_at datetime not null default current_timestamp on update current_timestamp,
-    deleted_at datetime,
-    primary key (id),
-    key idx_deleted_at (deleted_at)
-) engine = InnoDB default charset = utf8;
-
 create table workspace_user (
     id int unsigned not null auto_increment,
     workspace_id int unsigned not null,
