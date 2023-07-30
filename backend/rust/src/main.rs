0931a32f1027ccd7503f4c96bdbfbfa0ba0928aa
T45K <tasktas9@gmail.com>
2023-07-30 11:54:48 +0900

    add db migration

--- backend/rust/src/main.rs
+++ backend/rust/src/main.rs
@@ -1,3 +1,13 @@
-fn main(){
-  println!("hello world")
+use sqlx::MySqlPool;
+
+#[tokio::main]
+async fn main() {
+    let pool = MySqlPool::connect(&"mysql://root:root@0.0.0.0:3306/hottocoffee")
+        .await
+        .unwrap();
+
+    sqlx::migrate!("./migrations")
+        .run(&pool)
+        .await
+        .unwrap();
 }
