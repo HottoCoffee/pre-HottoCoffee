604a7870042fe4e3b5d90098882606ad10683204
T45K <tasktas9@gmail.com>
2023-07-30 11:57:25 +0900

    extract method

--- backend/rust/src/main.rs
+++ backend/rust/src/main.rs
@@ -2,6 +2,10 @@ use sqlx::MySqlPool;
 
 #[tokio::main]
 async fn main() {
+    migrate_db().await
+}
+
+async fn migrate_db() {
     let pool = MySqlPool::connect(&"mysql://root:root@0.0.0.0:3306/hottocoffee")
         .await
         .unwrap();
