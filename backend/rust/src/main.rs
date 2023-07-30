346d5f0377f2fd76456a3e28d8a3329d091ac304
T45K <tasktas9@gmail.com>
2023-07-30 16:21:38 +0900

    write main

--- backend/rust/src/main.rs
+++ backend/rust/src/main.rs
@@ -1,8 +1,27 @@
+use axum::Router;
+use axum::routing::post;
 use sqlx::MySqlPool;
+
+use crate::adopter::user_controller;
+
+mod adopter;
+mod infra;
+mod entity;
 
 #[tokio::main]
 async fn main() {
-    migrate_db().await
+    migrate_db().await;
+
+    let public_route = Router::new()
+        .route("/sign-in", post(user_controller::sign_in));
+
+    let route = Router::new()
+        .nest("/api/public", public_route);
+
+    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
+        .serve(route.into_make_service())
+        .await
+        .unwrap();
 }
 
 async fn migrate_db() {
