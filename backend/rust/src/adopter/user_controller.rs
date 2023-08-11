d8c77f12f888b03323cf76f97dea2ef0132be997
T45K <tasktas9@gmail.com>
2023-08-11 22:42:54 +0900

    add dummy sign up

--- backend/rust/src/adopter/user_controller.rs
+++ backend/rust/src/adopter/user_controller.rs
@@ -8,6 +8,7 @@ use crate::adopter::jwt_claims::Jwt;
 use crate::entity::repository::user_repository::UserRepository;
 use crate::infra::repository::user_repository_db_client::UserDbClient;
 use crate::infra::util::cryptor::Cryptor;
+use crate::infra::util::hashed_password::HashedPassword;
 
 use super::error_response::ErrorResponse;
 
@@ -26,6 +27,35 @@ pub async fn sign_in(State(pool): State<MySqlPool>, Json(request): Json<SignInUp
             return Err((StatusCode::NOT_FOUND, Json(not_found_response)));
         }
     }
+}
+
+pub async fn sign_up(State(pool): State<MySqlPool>, Json(request): Json<SignInUpRequest>)
+                     -> Result<Json<UserResponse>, (StatusCode, Json<ErrorResponse>)> {
+    let cryptor = Cryptor::new("pass".to_string());
+    let encrypted_email = cryptor.encrypt(&request.email);
+    let hashed_password = HashedPassword::from_plain_password(request.password);
+
+    let tx = pool.begin().await.unwrap();
+    sqlx::query!(
+        r#"
+        insert into user(display_name, email, password)
+        value ('user', ?, ?)
+        "#,
+        encrypted_email, hashed_password.value
+    )
+        .execute(&pool)
+        .await
+        .unwrap();
+
+    let user_id = sqlx::query_scalar("select last_insert_id()")
+        .fetch_one(&pool)
+        .await
+        .unwrap();
+
+    tx.commit().await.unwrap();
+
+    let jwt = Jwt::issue(user_id);
+    Ok(Json(UserResponse { token: jwt }))
 }
 
 #[derive(Deserialize)]
