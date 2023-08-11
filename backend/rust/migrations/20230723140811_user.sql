b7f8872e7f466dbd433fb044be65de075cb248d9
T45K <tasktas9@gmail.com>
2023-08-11 22:15:50 +0900

    update existing DB schema

--- backend/rust/migrations/20230723140811_user.sql
+++ backend/rust/migrations/20230723140811_user.sql
@@ -2,10 +2,11 @@ create table user (
     id int unsigned not null auto_increment,
     display_name varchar(255) not null,
     email blob not null,
-    password blob not null,
+    password char(60) not null,
     created_at datetime not null default current_timestamp,
     updated_at datetime not null default current_timestamp on update current_timestamp,
     deleted_at datetime,
     primary key (id),
-    key idx_deleted_at (deleted_at)
+    key idx_deleted_at (deleted_at),
+    unique uni_email (email(255))
 ) engine = InnoDB default charset = utf8;
