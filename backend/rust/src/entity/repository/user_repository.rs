558f6cbd7d68d318fa715cf5b27a78d35b85500e
T45K <tasktas9@gmail.com>
2023-08-13 16:12:52 +0900

    add repo

--- backend/rust/src/entity/repository/user_repository.rs
+++ backend/rust/src/entity/repository/user_repository.rs
@@ -5,4 +5,7 @@ use crate::entity::domain::user::User;
 #[async_trait]
 pub trait UserRepository {
     async fn find_by_email_and_password(&self, email: &String, password: &String) -> Option<User>;
+    async fn create(&self, email: &String, password: &String) -> Result<User, UserAlreadyExistError>;
 }
+
+pub struct UserAlreadyExistError {}
